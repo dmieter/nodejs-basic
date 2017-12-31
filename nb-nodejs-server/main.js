8d46b51c9c0f31821eee56cb0dc439c505580ef1
dmieter <dmieter@yahoo.com>
2017-12-31 18:09:02 +0300

    nodejs nb project

--- nb-nodejs-server/main.js
+++ nb-nodejs-server/main.js
@@ -7,18 +7,22 @@
 //server.listen(8080);
 
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('DMIETER.ORG!');
+});
 
-app.get('/andrewofwonderfulmoscow', function (req, res) {
-  res.send('Happy New Sukin-Bukin Andrew!')
-})
+app.get('/andyofwonderfulmoscow', function (req, res) {
+  res.send('Happy New Sukin-Bukin Andrew!');
+});
+
+app.get('/iness', function (req, res) {
+  res.send('Inessa! Happy Perfect New Year 2018! \r \n ( ͡° ͜ʖ ͡°)');
+});
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8000!')
-})
+  console.log('Example app listening on port 8080!');
+});
 
