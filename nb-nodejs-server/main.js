32d488218239baf18670e052eb4d6d5048d8069a
dmieter <dmieter@yahoo.com>
2017-12-31 17:56:43 +0300

    nodejs nb project

--- nb-nodejs-server/main.js
+++ nb-nodejs-server/main.js
@@ -1,9 +1,24 @@
-var http = require('http');
+//var http = require('http');
+//
+//var server = http.createServer(function(req, res) {
+//  res.writeHead(200);
+//  res.end('Happy New Sukin-Bukin Andrew');
+//});
+//server.listen(8080);
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Happy New Sukin-Bukin Andrew');
-});
-server.listen(8080);
 
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/andrewofwonderfulmoscow', function (req, res) {
+  res.send('Happy New Sukin-Bukin Andrew!')
+})
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!')
+})
 
